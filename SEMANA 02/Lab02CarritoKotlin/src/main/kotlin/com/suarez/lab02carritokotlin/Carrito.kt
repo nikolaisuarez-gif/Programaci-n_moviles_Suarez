5279ea27228deed5c342b240392f6230ea0878a0
Nikolai Suarez <nikolai.suarez@tecsup.edu.pe>
2026-08-26 00:13:06 -0500

    Proyecto inicial con el menú del carrito

---
+++ SEMANA 02/Lab02CarritoKotlin/src/main/kotlin/com/suarez/lab02carritokotlin/Carrito.kt
@@ -0,0 +1,7 @@
+package com.suarez.lab02carritokotlin
+
+fun main() {
+    println("=========================================")
+    println(" CARRITO DE COMPRAS - TIENDA TECSUP ")
+    println("=========================================")
+}
