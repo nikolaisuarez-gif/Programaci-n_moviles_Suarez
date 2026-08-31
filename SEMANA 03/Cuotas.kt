b726abcc1c53827b73054a2e49c3cf16ed6ded5f
Nikolai Suarez <nikolai.suarez@tecsup.edu.pe>
2026-08-31 00:31:02 -0500

    Agregando cuotas e intereses

--- SEMANA 03/Cuotas.kt
+++ SEMANA 03/Cuotas.kt
@@ -14,7 +14,25 @@ fun main() {
     print("Ingrese la cantidad: ")
     val cantidad = sc.nextInt()
 
-    println("\nProducto registrado: $producto")
-    println("Precio unitario: S/ $precio")
-    println("Cantidad: $cantidad")
+    // Selección de cuotas e interés
+    var cuotas = 0
+    var interesPorcentaje = 0.0
+
+    while (cuotas != 6 && cuotas != 12 && cuotas != 24) {
+        print("Ingrese el número de cuotas (6, 12 o 24): ")
+        cuotas = sc.nextInt()
+
+        interesPorcentaje = when (cuotas) {
+            6 -> 0.20
+            12 -> 0.40
+            24 -> 0.60
+            else -> {
+                println("Error: Las cuotas solo pueden ser 6, 12 o 24. Intente de nuevo.")
+                0.0
+            }
+        }
+    }
+
+    println("\nCuotas seleccionadas: $cuotas")
+    println("Interés aplicado: ${interesPorcentaje * 100}%")
 }
