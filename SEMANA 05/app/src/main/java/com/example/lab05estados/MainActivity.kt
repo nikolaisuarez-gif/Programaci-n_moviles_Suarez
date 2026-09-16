b25056f0e73d327d4db8728d1ce24b7abf0c7768
Nikolai Suarez <nikolai.suarez@tecsup.edu.pe>
2026-09-16 11:22:46 -0500

    Actividad 1 - Commit 4: Organización de imports y limpieza

--- SEMANA 05/app/src/main/java/com/example/lab05estados/MainActivity.kt
+++ SEMANA 05/app/src/main/java/com/example/lab05estados/MainActivity.kt
@@ -9,7 +9,9 @@ import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
