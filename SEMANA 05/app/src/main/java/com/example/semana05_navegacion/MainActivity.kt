7137d08f7f748ec017a8e05adc1570d15a8018cc
Nikolai Suarez <nikolai.suarez@tecsup.edu.pe>
2026-09-23 11:00:11 -0500

    feat(fit): 8/8 conecta FitAppNav en MainActivity

--- SEMANA 05/app/src/main/java/com/example/semana05_navegacion/MainActivity.kt
+++ SEMANA 05/app/src/main/java/com/example/semana05_navegacion/MainActivity.kt
@@ -4,14 +4,15 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import com.example.semana05_navegacion.navigation.AppNavigation
+import com.example.semana05_navegacion.tecsupfit.navigation.FitAppNav
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            AppNavigation()
+            // Muestra TECSUP Fit (Opción B)
+            FitAppNav()
         }
     }
 }
